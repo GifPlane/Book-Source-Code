1f1b248afc40d997e22c883c3e14d4c64e074201
SeaHandSome <itact@hotmail.com>
2018-06-05 11:39:19 +0800

    update chapter3_01

---
+++ Microsoft Visual Cs 2013 step by step/Chapter3_01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartI
+{
+    class Chapter3_01
+    {
+        static void Main(string[] args)
+        {
+            (new Chapter3_01()).run();
+        }
+
+        void run()
+        {
+            double dailyRate = readDouble("Enter your daily rate: ");
+            int noOfDays = readInt("Enter your number of days: ");
+            writeFee(calculateFee(dailyRate, noOfDays));
+        }
+
+        private void writeFee(double p)
+        {
+            Console.WriteLine("The consultant's fee is: {0}", p * 1.1); //{0} 表示第一个，号之后的参数
+        }
+
+        private double calculateFee(double dailyRate, int noOfDays)
+        {
+            return dailyRate * noOfDays;
+        }
+
+        private int readInt(string p)
+        {
+            Console.Write(p);
+            string line = Console.ReadLine();
+            return int.Parse(line);
+        }
+
+        private double readDouble(string p)
+        {
+            Console.Write(p);
+            string line = Console.ReadLine();
+            return double.Parse(line);
+        }
+    }
+}
