d4a9282afbe8c0ba64fa3ab38dfb4437729922e5
SeaHandSome <itact@hotmail.com>
2018-06-06 09:57:28 +0800

    update

---
+++ Microsoft Visual Cs 2013 step by step/Chapter8/Chapter8_01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Pass
+    {
+
+        public static void Main(String[] args)
+        {
+            doWork();
+        }
+
+        public static void Value(int param)
+        {
+            param = 42;
+        }
+
+        static void doWork()
+        {
+            int i = 0;
+            Console.WriteLine(i);
+            Pass.Value(i);
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Microsoft Visual Cs 2013 step by step/Chapter8/Chapter8_02.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    在C#中使用执政指针，使用unsafe关键字
+ */
+
+namespace Part2
+{
+    class Chapter8_02
+    {
+        public static void Main(String[] args)
+        {
+            int x = 99, y = 100;
+            unsafe
+            {
+                swap(&x, &y);
+            }
+            
+        }
+
+        public static unsafe void swap(int* a, int* b)
+        {
+            int temp;
+            temp = *a;
+            *a = *b;
+            *b = temp;
+        }
+    }
+}
